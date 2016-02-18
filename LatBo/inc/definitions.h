6fe8e8a8cf8be893de8a25875b44cae6b047be6b
Joe O'Connor <joseph.oconnor@postgrad.manchester.ac.uk>
2016-02-18 12:03:55 +0000

    Set up case for comparison with old code

--- LatBo/inc/definitions.h
+++ LatBo/inc/definitions.h
@@ -61,7 +61,7 @@ const static int zProbeLims[2] = {30, 120};
 // Gravity
 #define GRAVITY_ON
 // Expression for the gravity force
-#define grav_force 1e-2	//( 3 * gUtils.vecnorm(u_0x,u_0y,u_0z) * nu / pow(fabs(b_y - a_y),2) )
+#define grav_force 1e-1	//( 3 * gUtils.vecnorm(u_0x,u_0y,u_0z) * nu / pow(fabs(b_y - a_y),2) )
 #define grav_direction 0	// Gravity direction (0 = x, 1 = y, 2 = z)
 
 // Initialisation
@@ -86,7 +86,7 @@ const static int zProbeLims[2] = {30, 120};
 ***************************************************************************************************************
 */
 
-#define T 2	// Number of time steps
+#define T 1	// Number of time steps
 
 
 /*
@@ -204,7 +204,7 @@ const static size_t zRankSize[Xcores*Ycores*Zcores]		= {20, 30, 20, 30, 20, 30, 
 
 // Mechanical properties of filament
 #define ibb_delta_rho 1.0	// Difference in density (lattice units) between solid and fluid
-#define ibb_EI 0.001			// Flexural rigidity (lattice units) of filament
+#define ibb_EI 0.01			// Flexural rigidity (lattice units) of filament
 
 
 /*

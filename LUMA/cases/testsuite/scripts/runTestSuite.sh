819c0d6a057a09cc840519cb97fdeed2b347e6d2
Joe O'Connor <joseph.oconnor@manchester.ac.uk>
2016-11-02 15:18:24 +0000

    Added rebase feature to test suite script

--- LUMA/cases/testsuite/scripts/runTestSuite.sh
+++ LUMA/cases/testsuite/scripts/runTestSuite.sh
@@ -13,7 +13,6 @@ DIR_INC=${DIR_HDF5}/include						# Include directory
 DIR_LIB=${DIR_HDF5}/lib							# Library path
 LIB=hdf5										# Library
 
-
 # Set up the variables containing the paths to directories we need
 DIR_WORKING=..							# Working directory
 DIR_BASE=${DIR_WORKING}/base			# Base directory containing reference results
@@ -28,39 +27,46 @@ LOG_FILE=testsuite.log
 # File for diff comparison
 DIFF_FILE=io_lite.Lev0.Reg0.Rnk0.100.dat
 
-
 # If running with the clean option it will just delete the results folder
+OPTION="none"
 while [ ! $# -eq 0 ]
 do
 	case "$1" in
-		--clean | clean | -c)
+		--clean)
 			rm -rf ${DIR_RES}
 			rm -f ${DIR_WORKING}/${LOG_FILE}
 			exit
 			;;
+		--rebase)
+			OPTION="rebase"
+			rm -rf ${DIR_BASE}
+			mkdir ${DIR_BASE}
+			;;
 	esac
 	shift
 done
-
 
 # Delete results directory if it already exists and create it again
 rm -rf ${DIR_RES}
 mkdir ${DIR_RES}
 rm -f ${DIR_WORKING}/${LOG_FILE}
 
-
 # Get the number of cases to test and print to screen
 NCASES=`find ${DIR_DEF} -maxdepth 1 -type f | wc -l`
 printf "\n********** LUMA TEST SUITE **********\n"
-printf "Beginning test suite -> there are ${NCASES} cases to test\n\n"
+
+# Print out starting message
+if [ ${OPTION}  == "rebase" ]; then
+	printf "Beginning rebasing -> there are ${NCASES} cases to rebase\n\n"
+elif [ ${OPTION} == "none" ]; then
+	printf "Beginning test suite -> there are ${NCASES} cases to test\n\n"
+fi
 
 # Print header for master log file
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 printf "\n********** LUMA TEST SUITE - ${DATE} **********\n\n" > ${DIR_WORKING}/${LOG_FILE}
 
-
 # Loop through all cases
-#for CASE_DEF_PATH in ${DIR_DEF}/definitions_008.h
 for CASE_DEF_PATH in ${DIR_DEF}/*
 do
 
@@ -69,10 +75,8 @@ do
 	CASE_NUM=${CASE_NUM#*_}
 	CASE_NUM_INT=$((10#${CASE_NUM}))
 
-
 	# Inform user we are starting this case (use expansion to treat CASE_NUM as number rather than string)
 	printf "Starting case ${CASE_NUM_INT} of $((NCASES-1))...\n"
-
 
 	# Create a directory for this case in the results directory and copy the definitions file there (in case it needs to be inspected)
 	CASE_RES_PATH=${DIR_RES}/case${CASE_NUM}
@@ -83,55 +87,59 @@ do
 	# IB point cloud cases
 	if [ ${CASE_NUM_INT} -eq 0 ] || [ ${CASE_NUM_INT} -eq 1 ]; then
 		mkdir -p ${CASE_RES_PATH}/input
-		cp ${DIR_INPUT}/ibb_input.in ${CASE_RES_PATH}/input/.
+		cp ${DIR_INPUT}/ibb_input2D.in ${CASE_RES_PATH}/input/ibb_input.in
+	fi
+	if [ ${CASE_NUM_INT} -eq 30 ] || [ ${CASE_NUM_INT} -eq 31 ]; then
+		mkdir -p ${CASE_RES_PATH}/input
+		cp ${DIR_INPUT}/ibb_input3D.in ${CASE_RES_PATH}/input/ibb_input.in
 	fi
 
 	# BB point cloud cases
     if [ ${CASE_NUM_INT} -eq 3 ] || [ ${CASE_NUM_INT} -eq 6 ]; then
 		mkdir -p ${CASE_RES_PATH}/input
-		cp ${DIR_INPUT}/bbb_input.in ${CASE_RES_PATH}/input/.
+		cp ${DIR_INPUT}/bbb_input2D.in ${CASE_RES_PATH}/input/bbb_input.in
 	fi
 
 	# BFL point cloud cases
     if [ ${CASE_NUM_INT} -eq 4 ] || [ ${CASE_NUM_INT} -eq 7 ]; then
 		mkdir -p ${CASE_RES_PATH}/input
-		cp ${DIR_INPUT}/bfl_input.in ${CASE_RES_PATH}/input/.
+		cp ${DIR_INPUT}/bfl_input2D.in ${CASE_RES_PATH}/input/bfl_input.in
 	fi
 
 	# Read in inlet profile
 	if [ ${CASE_NUM_INT} -eq 3 ] || [ ${CASE_NUM_INT} -eq 6 ]; then
 		mkdir -p ${CASE_RES_PATH}/input
-		cp ${DIR_INPUT}/inlet_profile.in ${CASE_RES_PATH}/input/.
+		cp ${DIR_INPUT}/inlet_profile2D.in ${CASE_RES_PATH}/input/inlet_profile.in
 	fi
 
 	# Copy restart data from previous case
-    if [ ${CASE_NUM_INT} -eq 1 ] || [ ${CASE_NUM_INT} -eq 9 ]; then
+    if [ ${CASE_NUM_INT} -eq 1 ] || [ ${CASE_NUM_INT} -eq 9 ] || [ ${CASE_NUM_INT} -eq 31 ]; then
 	    mkdir -p ${CASE_RES_PATH}/input
 		LAST_CASE=$((${CASE_NUM_INT}-1))			
 		LAST_CASE=$(printf %03d ${LAST_CASE})
-		LAST_DIR_OUT=`find ${DIR_RES}/case${LAST_CASE} -maxdepth 1 -type d -name 'output*'`
+		if [ ${OPTION}  == "rebase" ]; then
+			LAST_DIR_OUT=${DIR_BASE}/case${LAST_CASE}
+		elif [ ${OPTION} == "none" ]; then
+			LAST_DIR_OUT=`find ${DIR_RES}/case${LAST_CASE} -maxdepth 1 -type d -name 'output*'`
+		fi
 		cp ${LAST_DIR_OUT}/restart* ${CASE_RES_PATH}/input/.
 	fi
 
 	# Number of processes to run on
-	if [ ${CASE_NUM_INT} -eq 0 ] || [ ${CASE_NUM_INT} -eq 1 ] || [ ${CASE_NUM_INT} -eq 2 ] || [ ${CASE_NUM_INT} -eq 3 ] || [ ${CASE_NUM_INT} -eq 4 ] || [ ${CASE_NUM_INT} -eq 5 ]; then
+	if [ ${CASE_NUM_INT} -eq 0 ] || [ ${CASE_NUM_INT} -eq 1 ] || [ ${CASE_NUM_INT} -eq 2 ] || [ ${CASE_NUM_INT} -eq 3 ] || [ ${CASE_NUM_INT} -eq 4 ] || [ ${CASE_NUM_INT} -eq 5 ] || [ ${CASE_NUM_INT} -eq 30 ] || [ ${CASE_NUM_INT} -eq 31 ]; then
 		NPROCS=1
 	elif [ ${CASE_NUM_INT} -eq 6 ] || [ ${CASE_NUM_INT} -eq 7 ] || [ ${CASE_NUM_INT} -eq 8 ] || [ ${CASE_NUM_INT} -eq 9 ]; then
 		NPROCS=8
 	fi
 
-
 	# Copy the definition file into the inc directory in the LUMA source folder
     cp ${CASE_DEF_PATH} ${DIR_LUMA}/inc/definitions.h
-
 
 	# Get the path to the soon to be created executable
 	CASE_EXE=${CASE_RES_PATH}/${EXE}${CASE_NUM}
 
-
 	# Starting compiling
 	printf "Compiling..."
-
 
 	# Compile the source files and test for success
 	if ${CC} ${CFLAGS} -I${DIR_INC} ${DIR_LUMA}/src/* -o ${CASE_EXE} -L${DIR_LIB} -l${LIB} &> ${CASE_RES_PATH}/compile.log; then
@@ -151,33 +159,44 @@ do
 			# Get the path to the output directory
 			DIR_OUT=`find ${CASE_RES_PATH} -maxdepth 1 -type d -name 'output*'`
 
-			# Checking results
-			printf "Runnning a diff on the results..."
+			# If rebasing then move this to the base directory
+			if [ ${OPTION} == "rebase" ]; then
+				mv ${DIR_OUT} ${DIR_BASE}/case${CASE_NUM}
+				mv ${CASE_RES_PATH}/* ${DIR_BASE}/case${CASE_NUM}
+				printf "Case ${CASE_NUM_INT} has been rebased successfully!\n\n"
+				printf "CASE ${CASE_NUM_INT} -> REBASED SUCCESSFULLY\n" >> ${DIR_WORKING}/${LOG_FILE}
+			
+			# If not rebasing then perform the diff on the results			
+			elif [ ${OPTION} == "none" ]; then
 
-			# Perform the diff and collect the output
-			if diff -q ${DIR_BASE}/case${CASE_NUM}/${DIFF_FILE} ${DIR_OUT}/${DIFF_FILE} > /dev/null; then
+				# Checking results
+				printf "Runnning a diff on the results..."
 
-				# Check passed
-				printf "success!\n"
-				printf "Case ${CASE_NUM_INT} has passed!\n\n"
+				# Perform the diff and collect the output
+				if diff -q ${DIR_BASE}/case${CASE_NUM}/${DIFF_FILE} ${DIR_OUT}/${DIFF_FILE} > /dev/null; then
 
-				# Write to master log file
-				printf "CASE ${CASE_NUM_INT} -> PASS\n" >> ${DIR_WORKING}/${LOG_FILE}
+					# Check passed
+					printf "success!\n"
+					printf "Case ${CASE_NUM_INT} has passed!\n\n"
 
-			else
+					# Write to master log file
+					printf "CASE ${CASE_NUM_INT} -> PASS\n" >> ${DIR_WORKING}/${LOG_FILE}
 
-				# Check failed
-				printf "failed (check diff log file)\n"
+				else
 
-				# Write out the diff log
-				printf "Writing diff log in case folder..."
-				diff -y -W 500 ${DIR_BASE}/case${CASE_NUM}/${DIFF_FILE} ${DIR_OUT}/${DIFF_FILE} > ${CASE_RES_PATH}/diff.log
+					# Check failed
+					printf "failed (check diff log file)\n"
 
-				# Print status
-				printf "Case ${CASE_NUM_INT} failed!\n\n"
+					# Write out the diff log
+					printf "Writing diff log in case folder..."
+					diff -y -W 500 ${DIR_BASE}/case${CASE_NUM}/${DIFF_FILE} ${DIR_OUT}/${DIFF_FILE} > ${CASE_RES_PATH}/diff.log
 
-				# Write to master log file
-				printf "CASE ${CASE_NUM_INT} -> FAILED ON DIFF\n" >> ${DIR_WORKING}/${LOG_FILE}
+					# Print status
+					printf "Case ${CASE_NUM_INT} failed!\n\n"
+
+					# Write to master log file
+					printf "CASE ${CASE_NUM_INT} -> FAILED ON DIFF\n" >> ${DIR_WORKING}/${LOG_FILE}
+				fi
 			fi
 		else
 
@@ -199,4 +218,10 @@ do
 	fi
 done
 
-printf "Finished test suite\n\n"
+# Write out finished message
+if [ ${OPTION}  == "rebase" ]; then
+	rm -rf ${DIR_RES}
+	printf "Finished rebasing test suite\n\n"
+elif [ ${OPTION} == "none" ]; then
+	printf "Finished test suite\n\n"
+fi

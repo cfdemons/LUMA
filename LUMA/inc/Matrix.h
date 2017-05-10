f1cacde2a3735b6b8aea4b3b160b3201cd87d4a3
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2017-05-10 17:26:49 +0000

    Matrix.h edited online with Bitbucket

--- LUMA/inc/Matrix.h
+++ LUMA/inc/Matrix.h
@@ -1,36 +1,49 @@
 /*
-* --------------------------------------------------------------
-*
-* ------ Lattice Boltzmann @ The University of Manchester ------
-*
-* -------------------------- L-U-M-A ---------------------------
-*
-*  Copyright (C) 2015, 2016
-*  E-mail contact: info@luma.manchester.ac.uk
-*
-* This software is for academic use only and not available for
-* distribution without written consent.
-*
-*/
+ * --------------------------------------------------------------
+ *
+ * ------ Lattice Boltzmann @ The University of Manchester ------
+ *
+ * -------------------------- L-U-M-A ---------------------------
+ *
+ *  Copyright (C) The University of Manchester 2017
+ *  E-mail contact: info@luma.manchester.ac.uk
+ *
+ * This software is for academic use only and not available for
+ * further distribution commericially or otherwise without written consent.
+ *
+ */
 
 #pragma once
 
 template<typename T> class Matrix2D
 {
 public:
-	Matrix2D(){}
+	Matrix2D() {}
 	Matrix2D(int rows, int cols)
 	{
 		MatrixResize(rows, cols);
 	}
-	~Matrix2D(){}
+	~Matrix2D() {}
 
-	size_t GetCols(){ return m_cols; }
-	size_t GetRows(){ return m_rows; }
-	std::valarray<T>& operator[](int i) { return m_values[i]; }
-	std::valarray<T> operator[](int i) const { return m_values[i]; }
+	size_t GetCols()
+	{
+		return m_cols;
+	}
+	size_t GetRows()
+	{
+		return m_rows;
+	}
+	std::valarray<T>& operator[](int i)
+	{
+		return m_values[i];
+	}
+	std::valarray<T> operator[](int i) const
+	{
+		return m_values[i];
+	}
 
-	void MatrixResize(int rows, int cols)          //Resizes the matrix. It will erase all its content. 
+	// Resizes the matrix. It will erase all its content. 
+	void MatrixResize(int rows, int cols)          
 	{
 		m_values.resize(rows);
 		for (auto it = begin(m_values); it != end(m_values); ++it)
@@ -42,12 +55,14 @@ public:
 
 	Matrix2D<T>& operator=(const Matrix2D<T>& other)
 	{
-		//WHAT HAPPENS IF THE SIZES OF THE ARRAYS ARE NTO THE SAME? WILL THE EQUAL FROM VALARRAY THROW AN EXCEPTION? OR DO THIS FUNCTION HAS TO THROW IT?
+		// TODO: Handle case when matrices are not the same size
 		for (int i = 0; i < m_rows; ++i)
 			m_values[i] = other[i];
 		return *this;
 	}
-	T operator%(const Matrix2D<T>& other) const      //Inner product
+	
+	// Inner product operator
+	T operator%(const Matrix2D<T>& other) const
 	{
 		std::valarray<T> aux(m_rows);
 		std::valarray<T> auxCol(m_cols);
@@ -62,7 +77,7 @@ public:
 private:
 	size_t m_cols;
 	size_t m_rows;
-	std::valarray<std::valarray<T>> m_values;
+	std::valarray< std::valarray<T> > m_values;
 };
 
 

3b293a9ddc1eea0eb5e0e777b7668ea8f3784039
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2017-08-21 12:13:28 +0100

    Updated header

--- LUMA/tools/post_processors/h5mgm/src/VelocitySorter.h
+++ LUMA/tools/post_processors/h5mgm/src/VelocitySorter.h
@@ -1,3 +1,17 @@
+/*
+* --------------------------------------------------------------
+*
+* ------ Lattice Boltzmann @ The University of Manchester ------
+*
+* -------------------------- L-U-M-A ---------------------------
+*
+*  Copyright (C) The University of Manchester 2017
+*  E-mail contact: info@luma.manchester.ac.uk
+*
+* This software is for academic use only and not available for
+* further distribution commericially or otherwise without written consent.
+*
+*/
 
 #include "h5mgm.h"
 

7b063832b801584ab2eb3ee375bfae48eeaa24b9
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2017-12-07 10:59:32 +0000

    Added License information and new boilerplate header for SoftwareX

--- LUMA/inc/MarkerData.h
+++ LUMA/inc/MarkerData.h
@@ -5,13 +5,21 @@
 *
 * -------------------------- L-U-M-A ---------------------------
 *
-*  Copyright (C) The University of Manchester 2017
-*  E-mail contact: info@luma.manchester.ac.uk
+* Copyright 2018 The University of Manchester
 *
-* This software is for academic use only and not available for
-* further distribution commericially or otherwise without written consent.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
 *
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.*
 */
+
 #ifndef MARDAT_H
 #define MARDAT_H
 
--- LUMA/src/GridObj_ops_io.cpp
+++ LUMA/src/GridObj_ops_io.cpp
@@ -1,17 +1,24 @@
 /*
- * --------------------------------------------------------------
- *
- * ------ Lattice Boltzmann @ The University of Manchester ------
- *
- * -------------------------- L-U-M-A ---------------------------
- *
- *  Copyright (C) The University of Manchester 2017
- *  E-mail contact: info@luma.manchester.ac.uk
- *
- * This software is for academic use only and not available for
- * further distribution commericially or otherwise without written consent.
- *
- */
+* --------------------------------------------------------------
+*
+* ------ Lattice Boltzmann @ The University of Manchester ------
+*
+* -------------------------- L-U-M-A ---------------------------
+*
+* Copyright 2018 The University of Manchester
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.*
+*/
 
 // Routines for reading and writing operations.
 

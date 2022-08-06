2a403cb0fcf96858d867e4295050117dcd2301e4
Ian Hinder <ian.hinder@manchester.ac.uk>
2022-08-06 16:26:57 +0100

    TimingWriter: Change to physical time for speed, clean up code, fix speed calculation bug

--- LUMA/inc/TimingWriter.h
+++ LUMA/inc/TimingWriter.h
@@ -30,15 +30,17 @@ class TimingWriter
 {
 public:
 
-  TimingWriter(const std::string outdir)
-    : t_start(std::chrono::steady_clock::now()),
-      t(-1),
-      runtime_s(-1),
-      speed(-1),
-      outfile(outdir+"/timing.txt") {
+  TimingWriter(const std::string outdir, double dt)
+    : outfile(outdir+"/timing.txt"),
+      dt(dt),
+      t_start(std::chrono::steady_clock::now()),
+      t(0),
+      runtime_s(0),
+      speed(0),
+      average_speed(0) {
     std::ofstream   os(outfile, std::ofstream::app);
-    os << "timestep\truntime[s]\tspeed_s[timesteps/s]\taverage_speed_s[timesteps/s]\taverage_speed_h[timesteps/h]" << std::endl;
-    writeTimingData(0, 0.0, 0.0, 0.0, 0.0);
+    os << "timestep\tphysical_time\truntime[s]\taverage_speed_h[physical_time/h]" << std::endl;
+    writeTimingData();
   }
 
   void recordTimingData(int t) {
@@ -46,30 +48,27 @@ public:
     int previous_t = this->t;
     auto t_now = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration = t_now - t_start;
-    runtime_s = duration.count();
+
     this->t = t;
-    speed = (runtime_s - previous_runtime_s) / (t - previous_t);
-    average_speed = runtime_s / t;
+    runtime_s = duration.count();
+    speed = (runtime_s - previous_runtime_s) / ((t - previous_t) * dt);
+    average_speed = (t * dt) / runtime_s;
   }
 
   void writeTimingData() {
-    writeTimingData(t, runtime_s, speed, average_speed, average_speed * 3600);
+    std::ofstream   os(outfile, std::ofstream::app);
+    os << t << "\t" << t * dt << "\t" << runtime_s << "\t" << average_speed*3600 << std::endl;
   }
 
 private:
-
-  void writeTimingData(int t, double runtime_s, double speed, double average_speed) {
-    std::ofstream   os(outfile, std::ofstream::app);
-    os << t << "\t" << runtime_s << "\t" << speed << "\t" << average_speed << std::endl;
-  }
-
+  std::string outfile;
+  double dt;
   std::chrono::time_point<std::chrono::steady_clock> t_start;
+
   int t;
   double runtime_s;
   double speed;
   double average_speed;
-  std::string outfile;
-  std::vector<double> runtime_history;
 };
 
 #endif
--- LUMA/src/main_lbm.cpp
+++ LUMA/src/main_lbm.cpp
@@ -436,7 +436,7 @@ int main( int argc, char* argv[] )
 #endif
 
 #ifdef L_WRITE_TIMING_DATA
-		TimingWriter timingWriter(GridUtils::path_str);
+		TimingWriter timingWriter(GridUtils::path_str, Grids->dt);
 #endif
 	
 	L_INFO("Initialising LBM time-stepping...", GridUtils::logfile);
